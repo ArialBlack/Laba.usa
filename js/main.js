35815f36190f4f0f5946adbc3d78686092362cad
ArialBlack <andreyturik@gmail.com>
2017-01-19 15:52:28 +0200

    modal

--- js/main.js
+++ js/main.js
@@ -24,4 +24,38 @@ $(document).ready(function() {
             //$('#about video').get(0).play();
         }
     });
+
+    $('.modal')
+        .on('show.bs.modal', function (e) {
+            var $this = $(this),
+                $btn = $(e.relatedTarget),
+                preloadSRC = $btn.data('preload');
+
+            $.fn.fullpage.setAllowScrolling(false);
+
+            if (preloadSRC) {
+                $this.find('.modal-body').load('/parts/' + preloadSRC, function( response, status, xhr ) {
+                    if ( status == "error" ) {
+                        var msg = "Sorry but there was an error: ";
+                        $("#error").html(msg + xhr.status + " " + xhr.statusText);
+                    }
+
+                    if ( status == "success" ) {
+                        $this.find('.modal-body .field-body').addClass('show');
+                        $this.find('.modal-body .preloader').addClass('hide');
+                    }
+                });
+            }
+    })
+        .on('hide.bs.modal', function (e) {
+            var $this = $(this),
+                $btn = $(e.relatedTarget);
+            
+            $.fn.fullpage.setAllowScrolling(true);
+            $this.find('.modal-body .field-body').removeClass('show');
+            $this.find('.modal-body .preloader').removeClass('hide');
+    });
+
+
+
 });
